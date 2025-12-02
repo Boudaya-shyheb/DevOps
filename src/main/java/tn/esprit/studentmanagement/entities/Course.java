8dbf2807e2244c16ab557537283e6b03db464193
Shyheb Boudaya <158149244+Shyheb-Boudaya@users.noreply.github.com>
2025-12-02 23:53:22 +0100

    1.0.15

--- src/main/java/tn/esprit/studentmanagement/entities/Course.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Course.java
@@ -16,7 +16,6 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCourse;
     private String name;
-    private String department;
     private String code;           // exemple : CS101
     private int credit;// nombre de crédits
     private String description;
